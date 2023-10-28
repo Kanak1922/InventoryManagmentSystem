06e6d70f8c5b676b83e744c484c75c9b3aee9ea2
Kanak Arya <kanakarya1922@gmail.com>
2023-10-28 20:32:08 +0530

    Initial commit of project

---
+++ InventoryManagmentSystem/src/test/java/com/kanak/ims/InventoryManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanak.ims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
